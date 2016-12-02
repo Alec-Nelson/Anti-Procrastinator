a48fa6e6e1e5c2e96921e47c12deffe8c298c1f8
Alec Nelson <Alec Nelson>
2016-12-02 17:54:59 -0500

    removed text and made new users redirect to google.

--- redirect.js
+++ redirect.js
@@ -1,3 +1,10 @@
+  //uses chrome synch storage to retrive url saved in popup
+  // if no url saved redirts to google.com
   chrome.storage.sync.get("url", function(items) {
-    window.location.href = items.url;
+    if (items.url == null) {
+      window.location.href = "https://www.google.com/";
+    }
+    else {
+      window.location.href = items.url;
+    }
   });
--- saveurl.js
+++ saveurl.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 
     $( "#enterButton" ).click(function() {
       chrome.storage.sync.set({ "url" : $("#inputText").val() }, function() {
-        alert( "Saving URL: " + $("#inputText").val());
+        // alert( "Saving URL: " + $("#inputText").val());
         if (chrome.runtime.error) {
         console.log("Runtime error.");
         }
